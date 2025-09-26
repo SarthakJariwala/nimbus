362e3de41f37b6428b9480bf66ede02ec384e750
Sarthak Jariwala <sarthak.jariwala@swiftsolar.com>
2025-09-26 14:13:17 -0700

    initial project setup

---
+++ frontend/js/components/Layout.svelte
@@ -0,0 +1,14 @@
+<script>
+import { Link } from "@inertiajs/svelte";
+</script>
+
+<div>
+    <nav class="flex items-start justify-center">
+        <ul class="flex space-x-4">
+            <li><Link href="/">Home</Link></li>
+        </ul>
+    </nav>
+    <div class="flex items-center justify-center mt-32">
+        <slot />
+    </div>
+</div>
---
+++ frontend/js/pages/Index.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Layout from "../components/Layout.svelte";
+</script>
+
+<Layout>
+    <div>
+        <h2 class="text-3xl font-bold underline">
+            This is the home page
+        </h2>
+        <p class="text-lg font-mono">
+            Find it in 'frontend/js/pages/Index.svelte'
+        </p>
+    </div>
+</Layout>
