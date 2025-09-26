f771066a953dd2057b0e49718d5c4cb267a3600a
Sarthak Jariwala <sarthak.jariwala@swiftsolar.com>
2025-09-26 14:50:34 -0700

    sample usage of auth protected inertia view

--- frontend/js/pages/Index.svelte
+++ frontend/js/pages/Index.svelte
@@ -1,11 +1,15 @@
 <script>
     import Layout from "../components/Layout.svelte";
+
+    const { user } = $props();
+
+    $inspect(user);
 </script>
 
 <Layout>
     <div>
         <h2 class="text-3xl font-bold underline">
-            This is the home page
+            Hi {user.username}! This is the home page
         </h2>
         <p class="text-lg font-mono">
             Find it in 'frontend/js/pages/Index.svelte'
--- nimbus/views.py
+++ nimbus/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from inertia import render
 
 
+@login_required
 def home(request):
-    return render(request, "Index")
+    return render(request, "Index", props={"user": request.user})
